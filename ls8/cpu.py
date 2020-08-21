cfaa4cc72ad972625472e6af596e4b89acce65fb
hsisco <hysensisco@gmail.com>
2020-08-21 14:05:54 -0400

    Day 4 MVP

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -19,7 +19,12 @@ class CPU:
             0b10000010: self.LDI,
             0b01000111: self.PRN,
             0b00000001: self.HLT,
-            0b10100010: self.MUL
+            0b10100010: self.MUL,
+            0b10100000: self.ADD,
+            0b01000101: self.PUSH,
+            0b01000110: self.POP,
+            0b01010000: self.CALL,
+            0b00010001: self.RET
         }
 
     # Should accept the address to read and return the value
@@ -40,6 +45,42 @@ class CPU:
 
     def HLT(self, i,v):
         self.running = False
+
+    def MUL(self, reg_a, reg_b):
+        self.alu('MUL', reg_a, reg_b)
+        self.pc += 3
+
+    def ADD(self, reg_a, reg_b):
+        self.alu('ADD', reg_a, reg_b)
+
+    def PUSH(self, *args):
+        self.reg[7] -= 1
+        register_index = self.ram[self.pc + 1]
+        self.ram[self.reg[7]] = self.reg[register_index]
+        self.pc += 2
+
+    def POP(self, *args):
+        register_index = self.ram[self.pc + 1]
+        self.reg[register_index] = self.ram[self.reg[7]]
+        self.reg[7] += 1
+        self.pc += 2
+
+    def CALL(self):
+        # Store next instruction in the stack
+        self.reg[7] -= 1
+        self.ram[self.reg[7]] = self.pc + 2
+        # Move program counter to address stored in register
+        register_index = self.ram[self.pc + 1]
+        self.pc = self.reg[register_index]
+
+    def RET(self):
+        # Pop from the stack
+        address = self.ram[self.reg[7]]
+        register_index = self.ram[self.pc + 1]
+        self.reg[register_index] = self.ram[self.reg[7]]
+        self.reg[7] += 1
+        # Update program counter
+        self.pc = address
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
@@ -63,10 +104,6 @@ class CPU:
                 except ValueError:
                     continue
 
-    def MUL(self, reg_a, reg_b):
-        self.alu('MUL', reg_a, reg_b)
-        self.pc += 3
-
     def trace(self):
         """
         Handy function to print out the CPU state. You might want to call this
@@ -75,8 +112,6 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
